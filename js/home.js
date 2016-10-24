7e1f6779692906c467a96964992765d25b4cb674
rosiezou <rosiezou@gmail.com>
2016-10-23 22:41:54 -0400

    small changes

--- js/home.js
+++ js/home.js
@@ -1,13 +1,11 @@
   function toggleMe(a){
   var e=document.getElementById(a);
-  var i = document.getElementById(a + '_image');
+  var i = document.getElementById(a);
   if(!e)return true;
   if(e.style.display=="none"){
     e.style.display="block"
-    i.src = 'YOUR OPEN IMAGE URL';
   } else {
     e.style.display="none"
-    i.src = 'YOUR CLOSED IMAGE URL';
   }
   return false;
   }
